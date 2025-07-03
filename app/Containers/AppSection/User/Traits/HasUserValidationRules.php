5653bf708497e08bbc0d01851854b32f996bf01c
Cheren <kalistratov.s.m@gmail.com>
2025-07-03 18:54:19 +0400

    HasUserValidationRules trait - add getUserExistsInOrganizationValidationRule method

--- app/Containers/AppSection/User/Traits/HasUserValidationRules.php
+++ app/Containers/AppSection/User/Traits/HasUserValidationRules.php
@@ -15,9 +15,12 @@
 namespace App\Containers\AppSection\User\Traits;
 
 use App\Containers\AppSection\Authorization\Models\Role;
+use App\Containers\AppSection\User\Foundation\User;
+use App\Containers\AppSection\User\Models\User as UserModel;
 use App\Ship\Collections\ValidationRulesCollection;
 use App\Ship\Validation\Rule;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Validation\Rules\Exists;
 
 trait HasUserValidationRules
 {
@@ -83,5 +86,11 @@ trait HasUserValidationRules
     public function getUserSurnameValidationRules(): ValidationRulesCollection
     {
         return validation_rules(config('appSection-user.rules.surname'));
+    }
+
+    public function getUserExistsInOrganizationValidationRule(mixed $organizationId): Exists
+    {
+        return Rule::exists(UserModel::TABLE, ID)
+            ->where(User::ORGANIZATION_ID, $organizationId);
     }
 }
