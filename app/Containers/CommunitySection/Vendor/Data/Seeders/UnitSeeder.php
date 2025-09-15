ee11e70c5b2987be37945b6f279f79c08e8e92ef
Cheren <kalistratov.s.m@gmail.com>
2025-09-15 17:35:31 +0400

    Add vendor UnitSeeder

---
+++ app/Containers/CommunitySection/Vendor/Data/Seeders/UnitSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * ERP system
+ *
+ * This file is part of the ERM system package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     https://kalistratov.ru/licenses/erp Proprietary license
+ * @copyright   Copyright (C) kalistratov.ru, All rights reserved ©.
+ * @link        https://kalistratov.ru
+ * @author      Sergey Kalistratov <sergey@kalistratov.ru>
+ */
+
+namespace App\Containers\CommunitySection\Vendor\Data\Seeders;
+
+use App\Containers\Vendor\Unit\Tasks\CreateUnitTask;
+use App\Ship\Exceptions\CreateResourceFailedException;
+use App\Ship\Parents\Seeders\Seeder;
+
+/**
+ * @codingStandardsIgnoreStart
+ */
+class UnitSeeder extends Seeder
+{
+    /**
+     * @throws CreateResourceFailedException
+     */
+    public function run(): void
+    {
+        $task = app(CreateUnitTask::class);
+        foreach ($this->getDefaultUnits() as $unitName) {
+            $task->run($unitName);
+        }
+    }
+
+    protected function getDefaultUnits(): array
+    {
+        return [
+            'шт.',
+            'упак.'
+        ];
+    }
+}
