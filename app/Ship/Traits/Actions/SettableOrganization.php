f4988a56071394608f2f597eb777d17778cf9a24
Cheren <kalistratov.s.m@gmail.com>
2025-09-19 13:47:48 +0400

    Add Ship/Traits/Actions/SettableOrganization.php

---
+++ app/Ship/Traits/Actions/SettableOrganization.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Beauty application system
+ *
+ * This file is part of the Beauty application system package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     Proprietary
+ * @copyright   Copyright (C) kalistratov.ru, All rights reserved.
+ * @link        https://kalistratov.ru
+ */
+
+namespace App\Ship\Traits\Actions;
+
+trait SettableOrganization
+{
+    protected ?int $organizationId = null;
+
+    public function organization(int $id): self
+    {
+        $this->organizationId = $id;
+        return $this;
+    }
+}
