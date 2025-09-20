eca6e3f188eacbace69d0e41971efc78ecd5f943
Cheren <kalistratov.s.m@gmail.com>
2025-09-20 18:18:38 +0400

    UpdateOrderAction - now can update items

--- app/Containers/OrderSection/Order/Actions/UpdateOrderAction.php
+++ app/Containers/OrderSection/Order/Actions/UpdateOrderAction.php
@@ -15,21 +15,62 @@
 
 namespace App\Containers\OrderSection\Order\Actions;
 
+use App\Containers\OrderSection\Item\Dto\UpdateItemDto;
+use App\Containers\OrderSection\Item\Foundation\Item;
+use App\Containers\OrderSection\Item\Tasks\UpdateItemTask;
 use App\Containers\OrderSection\Order\Dto\UpdateOrderDto;
 use App\Containers\OrderSection\Order\Models\Order;
+use App\Containers\OrderSection\Order\Tasks\CalculateOrderTotalTask;
 use App\Containers\OrderSection\Order\Tasks\UpdateOrderTask;
-use App\Ship\Parents\Actions\Action;
 use App\Ship\Exceptions\UpdateResourceFailedException;
+use App\Ship\Parents\Actions\Action;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
 class UpdateOrderAction extends Action
 {
     /**
      * @param UpdateOrderDto $dto
      * @return Order
+     * @throws UnknownProperties
      * @throws UpdateResourceFailedException
      */
     public function run(UpdateOrderDto $dto): Order
     {
-        return app(UpdateOrderTask::class)->run($dto);
+        $order = app(UpdateOrderTask::class)->run($dto);
+
+        $this->updateItems($order, $dto);
+        if ($dto->hasItems()) {
+            return app(CalculateOrderTotalTask::class)->run($order);
+        }
+
+        return $order;
+    }
+
+    /**
+     * @param Order $order
+     * @param UpdateOrderDto $dto
+     * @return void
+     * @throws UnknownProperties
+     * @throws UpdateResourceFailedException
+     */
+    protected function updateItems(Order $order, UpdateOrderDto $dto): void
+    {
+        if ($dto->hasItems()) {
+            collect($dto->items)
+                ->each(function (array $itemData) use ($order) {
+                    $itemData[Item::ORDER_ID] = $order->id;
+                    $this->updateItem(new UpdateItemDto($itemData));
+                });
+        }
+    }
+
+    /**
+     * @param UpdateItemDto $dto
+     * @return void
+     * @throws UpdateResourceFailedException
+     */
+    protected function updateItem(UpdateItemDto $dto): void
+    {
+        app(UpdateItemTask::class)->run($dto);
     }
 }
--- app/Containers/OrderSection/Order/Tests/Unit/Actions/UpdateOrderActionTest.php
+++ app/Containers/OrderSection/Order/Tests/Unit/Actions/UpdateOrderActionTest.php
@@ -15,8 +15,13 @@
 
 namespace App\Containers\OrderSection\Order\Tests\Unit\Actions;
 
+use App\Containers\CommunitySection\OrganizationUnit\Foundation\OrganizationUnit;
+use App\Containers\CommunitySection\OrganizationUnit\Models\OrganizationUnit as OrganizationUnitModel;
+use App\Containers\OrderSection\Item\Foundation\Item;
+use App\Containers\OrderSection\Item\Models\Item as ItemModel;
 use App\Containers\OrderSection\Order\Actions\UpdateOrderAction;
 use App\Containers\OrderSection\Order\Dto\UpdateOrderDto;
+use App\Containers\OrderSection\Order\Foundation\Order;
 use App\Containers\OrderSection\Order\Models\Order as OrderModel;
 use App\Containers\OrderSection\Order\Tests\UnitTestCase;
 use App\Ship\Exceptions\UpdateResourceFailedException;
@@ -37,13 +42,57 @@ final class UpdateOrderActionTest extends UnitTestCase
 
     public function testSuccess(): void
     {
-        $model = OrderModel::factory()->create();
-        $this->assertInstanceOf(OrderModel::class, $model);
+        $user = $this->getTestingOrganizationUser();
+
+        $unitA = OrganizationUnitModel::factory()
+            ->create([
+                OrganizationUnit::COST_PRICE => app('money')->addCurrency(100)->val(),
+                OrganizationUnit::CLIENT_PRICE => app('money')->addCurrency(210)->val(),
+                OrganizationUnit::ORGANIZATION_ID => $user->organization_id
+            ]);
+
+        $unitB = OrganizationUnitModel::factory()
+            ->create([
+                OrganizationUnit::COST_PRICE => app('money')->addCurrency(120)->val(),
+                OrganizationUnit::CLIENT_PRICE => app('money')->addCurrency(150)->val(),
+                OrganizationUnit::ORGANIZATION_ID => $user->organization_id
+            ]);
+
+        $order = OrderModel::factory()
+            ->create([
+                Order::ORGANIZATION_ID => $user->organization_id
+            ]);
+
+        $itemA = ItemModel::factory()
+            ->unit($unitA)
+            ->order($order)
+            ->create();
+
+        ItemModel::factory()
+            ->unit($unitB)
+            ->order($order)
+            ->create();
+
+        $order = $order->calculateTotal(true);
+
+        $this->assertSame(360.0, $order->total->currency()->val());
+        $this->assertCount(2, $order->items);
 
         $data = OrderModel::factory()
             ->make([
-                ID => $model->id,
-                //  write more.
+                ID => $order->id,
+                Order::TOTAL => 100,
+                Order::ITEMS => [
+                    [
+                        ID => $itemA->id,
+                        Item::NAME => $itemA->name,
+                        Item::UNIT_ID => $itemA->id,
+                        Item::SKU => $itemA->sku,
+                        Item::COST_PRICE => $itemA->cost_price->val(),
+                        Item::CLIENT_PRICE => $itemA->client_price->val(),
+                        Item::AMOUNT => 2
+                    ]
+                ]
             ]);
 
         $dto = new UpdateOrderDto($data->toArray());
@@ -51,5 +100,8 @@ final class UpdateOrderActionTest extends UnitTestCase
         $result = app(UpdateOrderAction::class)->run($dto);
 
         $this->assertInstanceOf(OrderModel::class, $result);
+
+        $this->assertSame(570.0, $result->total->currency()->val());
+        $this->assertCount(2, $result->items);
     }
 }
