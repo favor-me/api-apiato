58cf34143c5f655e8505360fae2dc150fd2afbe2
Sergey <kalistratov.s.m@gmail.com>
2026-02-17 21:10:31 +0400

    Add ship eloquent cast DateTimeAttribute.php

---
+++ app/Ship/Eloquent/Casts/DateTimeAttribute.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * FavorMe system
+ *
+ * This file is part of the FavorMe system package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://favor-me.ru/licenses/erp Proprietary license
+ * @copyright Copyright (C) kalistratov.ru, All rights reserved ©.
+ * @link https://kalistratov.ru
+ * @author Sergey Kalistratov <sergey@kalistratov.ru>
+ */
+
+namespace App\Ship\Eloquent\Casts;
+
+use Illuminate\Support\Carbon;
+
+class DateTimeAttribute
+{
+    public static function set(mixed $value): mixed
+    {
+        if (client_timezone() && !$value instanceof Carbon) {
+            return Carbon::createFromFormat(
+                DATE_TIME_FORMAT,
+                $value,
+                client_timezone()
+            )->utc();
+        }
+
+        return $value;
+    }
+
+    public static function get(mixed $value): mixed
+    {
+        if ($value instanceof Carbon) {
+            return $value->setTimezone(client_timezone());
+        }
+
+        return $value;
+    }
+}
