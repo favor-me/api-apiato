f197606aa2b3100fa7cf1089aa1989049265b546
Cheren <kalistratov.s.m@gmail.com>
2026-03-21 13:32:41 +0400

    Update Order ApiTestCase.php

--- app/Containers/OrderSection/Order/Tests/Functional/ApiTestCase.php
+++ app/Containers/OrderSection/Order/Tests/Functional/ApiTestCase.php
@@ -15,14 +15,49 @@
 
 namespace App\Containers\OrderSection\Order\Tests\Functional;
 
+use App\Containers\AppSection\User\Models\User;
+use App\Containers\AppSection\User\Models\User as UserModel;
 use App\Containers\OrderSection\Order\Tests\FunctionalTestCase;
 use App\Containers\OrderSection\Status\Foundation\Status;
 use App\Containers\OrderSection\Status\Models\Status as StatusModel;
+use App\Containers\ShiftSection\Shift\Foundation\Shift;
+use App\Containers\ShiftSection\Shift\Models\Shift as ShiftModel;
+use Illuminate\Support\Carbon;
 
 abstract class ApiTestCase extends FunctionalTestCase
 {
     public function getCompletedStatus(): StatusModel
     {
         return StatusModel::where(Status::SLUG, StatusModel::COMPLETED)->first();
+    }
+
+    public function getTestingOrganizationUser(?array $userDetails = null, ?array $access = null): UserModel
+    {
+        $nowShift = null;
+        if (array_key_exists('now_shift', (array)$userDetails)) {
+            $nowShift = $userDetails['now_shift'];
+            unset($userDetails['now_shift']);
+        }
+
+        $user = parent::getTestingOrganizationUser($userDetails, $access);
+
+        if ($nowShift  === true) {
+            $this->createUserNowShift($user);
+        }
+
+        return $user;
+    }
+
+    protected function createUserNowShift(UserModel $user): ShiftModel
+    {
+        $startAt = Carbon::now()->subHours(4);
+        $finishAt = Carbon::now()->addHours(4);
+
+        return ShiftModel::factory()
+            ->create([
+                Shift::START_AT => $startAt,
+                Shift::FINISH_AT => $finishAt,
+                CREATED_BY => $user->id
+            ]);
     }
 }
